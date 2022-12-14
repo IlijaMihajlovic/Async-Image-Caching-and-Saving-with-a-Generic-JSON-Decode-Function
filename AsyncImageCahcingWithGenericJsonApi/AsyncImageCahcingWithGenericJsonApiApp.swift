22b82122caff0d103a5860689906503f2b5a4de0
IlijaMihajlovic <ikacdrake619@gmail.com>
2022-12-14 13:48:01 +0100

    Initial Commit

---
+++ AsyncImageCahcingWithGenericJsonApi/AsyncImageCahcingWithGenericJsonApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageCahcingWithGenericJsonApiApp.swift
+//  AsyncImageCahcingWithGenericJsonApi
+//
+//  Created by Ilija Mihajlovic on 14.12.22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageCahcingWithGenericJsonApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImageCahcingWithGenericJsonApi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AsyncImageCahcingWithGenericJsonApi
+//
+//  Created by Ilija Mihajlovic on 14.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
